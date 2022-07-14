69508806d78bed36e7f2842315d28649d72e14dc
ctesenb <ctesenb@gmail.com>
2022-07-14 09:18:36 -0500

    Conexion cambiada neo-deter

--- Conexion.py
+++ Conexion.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "mysql+mysqlconnector://root:infoplus1234@localhost:3306/crudfastapi"
-DATABASE_URL = "mysql+mysqlconnector://admin:adbO1NJG@mysql-43314-0.cloudclusters.net:19751/neodeter"
+DATABASE_URL = "mysql+mysqlconnector://admin:adbO1NJG@mysql-43314-0.cloudclusters.net:19751/neo-deter"
 
 
 engine = create_engine(DATABASE_URL)
